14525d59d85075c313d73d854c926dbf26d10320
robby-robby <101137670+robby-robby@users.noreply.github.com>
2023-11-03 15:35:16 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -11,18 +11,11 @@ type config struct {
 	LogLevelStr  string
 	OpenAIApiKey string
 }
-type env string
 
-func (e env) IsDev() bool {
-	return strings.ToUpper(string(e)) == "DEV"
-}
-func (e env) IsProd() bool {
-	return strings.ToUpper(string(e)) == "PROD"
-}
 func NewConfig() *config {
 	c := &config{
 		Dsn:          getEnv("DSN", "file:heya.db"),
-		ENV:          env(strings.ToUpper(getEnv("ENV", "PROD"))),
+		ENV:          strings.ToUpper(getEnv("ENV", "PROD")),
 		LogLevelStr:  strings.ToUpper(getEnv("LOG_LEVEL", "DEBUG")),
 		OpenAIApiKey: mustGetEnv("OPENAI_API_KEY"),
 	}
@@ -36,13 +29,7 @@ func getEnv(key string, defV string) string {
 	return defV
 }
 
-// func getPath(rawurl string) string {
-// 	u, err := url.Parse(rawurl)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	return u.Path
-// }
+
 
 func mustGetEnv(key string) string {
 	if value, exists := os.LookupEnv(key); exists {
